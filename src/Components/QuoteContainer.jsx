8f030fd025311ffc5e0b1fee9ebad6db881ac8e5
DLipson <mdavidlipson@gmail.com>
2023-05-11 19:57:23 +0300

    remove redundant header

--- src/Components/QuoteContainer.jsx
+++ src/Components/QuoteContainer.jsx
@@ -3,10 +3,7 @@ import { Card, Button, Typography } from '@mui/material'
 
 const QuoteContainer = ({quotes, handleReloadClick}) => {
   return (
-    <>
-      <Typography variant='h5' align='center' >
-        Quotes
-      </Typography>      
+    <>     
       {quotes.map((quote, index) => (
         <Card key={index} sx={{p: 2}}>
             <Typography marginBottom={3} variant='body1' align='left'>{quote.quote}</Typography>
