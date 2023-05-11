704c281a7ecc7cd2da875f52c4c6edcba25dab10
DLipson <mdavidlipson@gmail.com>
2023-05-11 17:42:16 +0300

    add main app component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
+import { useState, useEffect } from "react";
+import { fetchQuotes } from "./Services/QuoteService";
+import { Header, QuoteContainer, Options, Footer } from "./Components/index";
+import { Stack, Container } from "@mui/material";
 
 
-function App() {
+function App() {    
+    const [category, setCategory] = useState("");
+    const [count, setCount] = useState(1);
+    const [quotes, setQuotes] = useState([]);
+    const maxCount = 10;
+       
+    const loadQuotes = async (category, count) => {
+        const quotes = await fetchQuotes(category, count);
+        setQuotes(quotes);
+    };
+
+    useEffect(() => {
+        loadQuotes(category, count);
+        console.log("useEffect");
+        }, [category, count]);
+    
   return (
-    <>
-    </>
+    <Container maxWidth="sm">
+      <Stack spacing={2}>
+        <Header />
+        <QuoteContainer
+          quotes={quotes}
+          handleReloadClick={loadQuotes}
+        />
+        <Options
+          category={category}
+          setCategory={setCategory}
+          count={count}
+          setCount={setCount}
+          countLimit={maxCount}
+        />
+        <Footer createdBy={"Dovid Lipson"} />
+      </Stack>
+    </Container>
   );
 }
 
