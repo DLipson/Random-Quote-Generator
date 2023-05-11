1e5491f43adecfcaf682982a6c09c94008ecfae9
DLipson <mdavidlipson@gmail.com>
2023-05-11 17:40:55 +0300

    add module to export custom components so they can all be imported in one line

---
+++ src/Components/index.js
@@ -0,0 +1,5 @@
+// export { Header, Footer, QuoteContainer, Options };
+export { default as Header } from './Header';
+export { default as Footer } from './Footer';
+export { default as QuoteContainer } from './QuoteContainer';
+export { default as Options } from './Options';
