d61166c996776e7e4429dc41d540c39b9eef4c86
DLipson <mdavidlipson@gmail.com>
2023-05-16 13:19:12 +0300

    limit skeleton screen max and and error info to UI

--- src/Components/QuoteContainer.jsx
+++ src/Components/QuoteContainer.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { Card, Button, Typography, Skeleton } from '@mui/material'
 
 const QuoteContainer = ({quotes, loading, handleReloadClick, count}) => {
+    const MAX_SKELETONS_TO_DISPLAY = 2;
   return (
     <>
       {loading
-        ? Array.from(new Array(count)).map(() => (
+        ? Array.from(new Array(count < MAX_SKELETONS_TO_DISPLAY ? count : MAX_SKELETONS_TO_DISPLAY )).map(() => (
             <Skeleton variant="rounded" animation="wave" height={150} />
           ))
-        : quotes.map((quote, index) => (
+        : (quotes.length !== 0 ? quotes.map((quote, index) => (
             <Card key={index} sx={{ p: 2 }}>
               <Typography marginBottom={3} variant="body1" align="left">
                 {quote.quote}
@@ -20,7 +21,7 @@ const QuoteContainer = ({quotes, loading, handleReloadClick, count}) => {
                 Category: {quote.category}
               </Typography>
             </Card>
-          ))}
+          )) : <Typography variant="body1" align="center">No quotes found</Typography>)}
       <Button variant="contained" onClick={() => handleReloadClick()}>
         Load New
       </Button>
